85b0ea8e09215ea4f29c45ffdf4ee13e15fb5f25
Christian <c.brueggemann94@gmail.com>
2015-11-02 18:50:29 +0100

    Neuer Test

--- HelloWeb/Startup.cs
+++ HelloWeb/Startup.cs
@@ -8,7 +8,8 @@ namespace HelloWeb
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
-            // app.UseStaticFiles();
+            app.UseStaticFiles();
+            // app.UseWelcomePage();
         }
     }
 }
