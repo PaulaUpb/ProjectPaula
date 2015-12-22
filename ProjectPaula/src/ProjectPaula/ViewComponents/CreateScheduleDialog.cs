d434ab62c15b731e50ead88e84e053620ad950ae
Christian <c.brueggemann94@gmail.com>
2015-12-22 14:44:31 +0100

    Refactor dialogs into their own files

---
+++ ProjectPaula/src/ProjectPaula/ViewComponents/CreateScheduleDialog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace ProjectPaula.ViewComponents
+{
+    public class CreateScheduleDialog : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+
+    }
+}
