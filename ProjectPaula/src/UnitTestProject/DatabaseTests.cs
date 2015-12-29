9dabf0f987cc8c4cb89b55634909336fd188f78e
MB <mburkhardt@live.de>
2015-12-29 17:54:31 +0100

    Added project for UnitTests, Added first test for database Modified DatabaseContext to accept custom filename

---
+++ ProjectPaula/src/UnitTestProject/DatabaseTests.cs
@@ -0,0 +1,41 @@
+﻿using ProjectPaula.DAL;
+using ProjectPaula.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTestProject
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class DatabaseTests
+    {
+        private DatabaseContext context = new DatabaseContext("TestDatabase.db");
+
+        public DatabaseTests()
+        {
+            CreateMockupDatabase();
+            PaulRepository.Filename = "TestDatabase.db";
+        }
+
+        private void CreateMockupDatabase()
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            context.Catalogues.Add(new CourseCatalog() { Title = "WS 15/16", InternalID = "1" });
+            context.SaveChanges();
+        }
+
+        [Fact]
+        public async Task ScheduleCreationTest()
+        {
+            context.Schedules.RemoveRange(context.Schedules);
+            context.SaveChanges();
+            await PaulRepository.CreateNewScheduleAsync(context.Catalogues.First());
+            Assert.True(context.Schedules.Any());
+
+        }
+    }
+}
