f73acb2c5873b98262bbe86d4bceae41750e8fa7
MB <mburkhardt@live.de>
2016-01-04 23:35:40 +0100

    Forgot to commit migration -.-

---
+++ ProjectPaula/src/ProjectPaula/Migrations/20160104174637_M3.Designer.cs
@@ -0,0 +1,206 @@
+using System;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Migrations;
+using ProjectPaula.DAL;
+
+namespace ProjectPaula.Migrations
+{
+    [DbContext(typeof(DatabaseContext))]
+    [Migration("20160104174637_M3")]
+    partial class M3
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "7.0.0-rc1-16348");
+
+            modelBuilder.Entity("ProjectPaula.Model.ConnectedCourse", b =>
+                {
+                    b.Property<string>("CourseId");
+
+                    b.Property<string>("CourseId2");
+
+                    b.HasKey("CourseId", "CourseId2");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.Course", b =>
+                {
+                    b.Property<string>("Id");
+
+                    b.Property<string>("CatalogueInternalID");
+
+                    b.Property<string>("CourseId");
+
+                    b.Property<string>("Docent");
+
+                    b.Property<string>("InternalCourseID");
+
+                    b.Property<bool>("IsConnectedCourse");
+
+                    b.Property<bool>("IsTutorial");
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("ShortName");
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("Id");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.CourseCatalog", b =>
+                {
+                    b.Property<string>("InternalID");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("InternalID");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.Date", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("CourseId");
+
+                    b.Property<DateTimeOffset>("From");
+
+                    b.Property<string>("Instructor");
+
+                    b.Property<string>("Room");
+
+                    b.Property<DateTimeOffset>("To");
+
+                    b.HasKey("Id");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.Log", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Date");
+
+                    b.Property<int>("Level");
+
+                    b.Property<string>("Message");
+
+                    b.Property<string>("Tag");
+
+                    b.HasKey("Id");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.Schedule", b =>
+                {
+                    b.Property<string>("Id");
+
+                    b.Property<string>("CourseCatalogueInternalID");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.SelectedCourse", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("CourseId");
+
+                    b.Property<string>("ScheduleId");
+
+                    b.HasKey("Id");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.SelectedCourseUser", b =>
+                {
+                    b.Property<int?>("UserId");
+
+                    b.Property<int?>("SelectedCourseId");
+
+                    b.HasKey("UserId", "SelectedCourseId");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.User", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("ScheduleId");
+
+                    b.HasKey("Id");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.ConnectedCourse", b =>
+                {
+                    b.HasOne("ProjectPaula.Model.Course")
+                        .WithMany()
+                        .HasForeignKey("CourseId");
+
+                    b.HasOne("ProjectPaula.Model.Course")
+                        .WithMany()
+                        .HasForeignKey("CourseId2");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.Course", b =>
+                {
+                    b.HasOne("ProjectPaula.Model.CourseCatalog")
+                        .WithMany()
+                        .HasForeignKey("CatalogueInternalID");
+
+                    b.HasOne("ProjectPaula.Model.Course")
+                        .WithMany()
+                        .HasForeignKey("CourseId");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.Date", b =>
+                {
+                    b.HasOne("ProjectPaula.Model.Course")
+                        .WithMany()
+                        .HasForeignKey("CourseId");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.Schedule", b =>
+                {
+                    b.HasOne("ProjectPaula.Model.CourseCatalog")
+                        .WithMany()
+                        .HasForeignKey("CourseCatalogueInternalID");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.SelectedCourse", b =>
+                {
+                    b.HasOne("ProjectPaula.Model.Course")
+                        .WithMany()
+                        .HasForeignKey("CourseId");
+
+                    b.HasOne("ProjectPaula.Model.Schedule")
+                        .WithMany()
+                        .HasForeignKey("ScheduleId");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.SelectedCourseUser", b =>
+                {
+                    b.HasOne("ProjectPaula.Model.SelectedCourse")
+                        .WithMany()
+                        .HasForeignKey("SelectedCourseId");
+
+                    b.HasOne("ProjectPaula.Model.User")
+                        .WithMany()
+                        .HasForeignKey("UserId");
+                });
+
+            modelBuilder.Entity("ProjectPaula.Model.User", b =>
+                {
+                    b.HasOne("ProjectPaula.Model.Schedule")
+                        .WithMany()
+                        .HasForeignKey("ScheduleId");
+                });
+        }
+    }
+}
---
+++ ProjectPaula/src/ProjectPaula/Migrations/20160104174637_M3.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Entity.Migrations;
+
+namespace ProjectPaula.Migrations
+{
+    public partial class M3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Schedule",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "Name", table: "Schedule");
+        }
+    }
+}
