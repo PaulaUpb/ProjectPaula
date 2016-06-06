5be55ecb9a6bd8c98d1bf998ad9a76ca26298d3a
Christian <c.brueggemann94@gmail.com>
2016-06-06 10:57:27 +0200

    Add support for command line flags for PAULa

--- ProjectPaula/src/ProjectPaula/Program.cs
+++ ProjectPaula/src/ProjectPaula/Program.cs
@@ -1,19 +1,63 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 
 namespace ProjectPaula
 {
     public class Program
     {
+
+        /// <summary>
+        /// Enum-like class containing type-safe flags the program
+        /// can be started with.
+        /// </summary>
+        sealed class Flag
+        {
+            /// <summary>
+            /// Flag indicating that Kestrel should listen on 0.0.0.0:80.
+            /// </summary>
+            public static readonly Flag ListenWeb = new Flag("listen-web");
+
+            private static readonly Flag[] Flags = { ListenWeb };
+
+            private readonly string _commandLineParam;
+
+            private Flag(string commandLineParam)
+            {
+                _commandLineParam = commandLineParam;
+            }
+
+            public static Flag ByCommandLineParam(string param)
+            {
+                return Flags.First(flag => flag._commandLineParam.Equals(param));
+            }
+
+            public static IEnumerable<Flag> FromCommandLineParams(IEnumerable<string> pars)
+            {
+                return pars.Select(par => Flags.FirstOrDefault(flag => par.Equals(flag._commandLineParam)))
+                    .Where(it => it != null);
+            }
+        }
+
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
+            var flags = Flag.FromCommandLineParams(args);
+
+            var hostBuilder = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
 
+            if (flags.Contains(Flag.ListenWeb))
+            {
+                hostBuilder.UseUrls("http://0.0.0.0:80");
+            }
+
+
+            var host = hostBuilder.Build();
             host.Run();
         }
     }
