2d7c46f308c6c6b2f9c2f6e7c936cdb207280382
Christian Brüggemann <mail@cbruegg.com>
2017-02-02 21:22:30 +0100

    Sort selected courses view

--- ProjectPaula/src/ProjectPaula/ViewModel/CourseListViewModel.cs
+++ ProjectPaula/src/ProjectPaula/ViewModel/CourseListViewModel.cs
@@ -11,7 +11,8 @@ namespace ProjectPaula.ViewModel
     public class CourseListViewModel : BindableBase
     {
         //Reuse the SearchResultViewModel
-        public ObservableCollectionEx<SearchResultViewModel> SelectedCourses { get; } = new ObservableCollectionEx<SearchResultViewModel>();
+        public ObservableCollectionEx<SearchResultViewModel> SelectedCourses { get; } 
+            = new ObservableCollectionEx<SearchResultViewModel>();
 
         private User _user;
         private Schedule _schedule;
@@ -28,7 +29,9 @@ namespace ProjectPaula.ViewModel
         public void UpdateCourseList()
         {
             SelectedCourses.Clear();
-            var courses = _schedule.SelectedCourses.Where(s => s.Users.Any(u => u.User == _user) && !s.Course.IsConnectedCourse).Select(s => s.Course);
+            var courses = _schedule.SelectedCourses
+                .Where(s => s.Users.Any(u => u.User == _user) && !s.Course.IsConnectedCourse).Select(s => s.Course)
+                .OrderBy(c => c.Name);
             SelectedCourses.AddRange(courses.Select(c => new SearchResultViewModel(c, true)));
         }
 
