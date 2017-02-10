1511db48907d525bae700e3429175fb623223b09
Christian <c.brueggemann94@gmail.com>
2017-02-10 11:00:19 +0100

    Fix ConnectedCourse.GetHashCode

--- ProjectPaula/src/ProjectPaula/Model/ConnectedCourse.cs
+++ ProjectPaula/src/ProjectPaula/Model/ConnectedCourse.cs
@@ -4,14 +4,26 @@
     {
         public string CourseId { get; set; }
         public string CourseId2 { get; set; }
+
+        protected bool Equals(ConnectedCourse other)
+        {
+            return string.Equals(CourseId, other.CourseId) && string.Equals(CourseId2, other.CourseId2);
+        }
+
         public override bool Equals(object obj)
         {
-            return obj is ConnectedCourse && ((ConnectedCourse)obj).CourseId2 == CourseId2 && ((ConnectedCourse)obj).CourseId == CourseId;
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ConnectedCourse) obj);
         }
 
         public override int GetHashCode()
         {
-            return CourseId2.GetHashCode() - CourseId.GetHashCode();
+            unchecked
+            {
+                return (CourseId.GetHashCode() * 397) ^ CourseId2.GetHashCode();
+            }
         }
     }
 
